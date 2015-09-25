d60e347c137eb993c439e9a3e4bcd79e0769d5c3
Itay Radotzki <radotzki@gmail.com>
2015-09-25 15:00:38 +0300

    Start with reveal the truth

--- src/app/game-components/reveal-the-truth/revealTheTruth.ts
+++ src/app/game-components/reveal-the-truth/revealTheTruth.ts
@@ -1,10 +1,9 @@
-import {
-Component, View,
-ControlGroup, FormBuilder, Validators,
-LifecycleEvent,
-FORM_DIRECTIVES, CORE_DIRECTIVES} from 'angular2/angular2';
+import {Component, View, LifecycleEvent} from 'angular2/angular2';
+import {APP_DIRECTIVES} from 'app/directives/index';
+import {ControlGroup, FormBuilder, Validators} from 'angular2/angular2';
 import {Router, RouteParams} from 'angular2/router';
 import * as _ from 'lodash';
+import {MDL_COMPONENTS, MdlService, LoadingMaskService, NotificationService} from 'app/mdl-components/index';
 
 import {IQuestion, QuestionState} from 'app/pof-typings/question';
 import {GameState} from 'app/pof-typings/game';
@@ -12,44 +11,56 @@ import {GameState} from 'app/pof-typings/game';
 import {Session} from 'app/session/session';
 import {GameApi} from 'app/datacontext/repositories/gameApi';
 
-let styles = require('./revealTheTruth.css');
-let template = require('./revealTheTruth.html');
+const styles = require('./revealTheTruth.css');
+const template = require('./revealTheTruth.html');
 
 const CURRENT_STATE = QuestionState.RevealTheTruth;
 const NEXT_STATE = QuestionState.ScoreBoard;
 const NEXT_STATE_ROUTE = '/score-board/';
-const REVEALING_THE_TRUTH_TIME = 5000;
+const REVEALING_THE_TRUTH_TIME = 5000000;
 
 @Component({
     selector: 'reveal-the-truth',
     lifecycle: [LifecycleEvent.OnDestroy]
 })
 @View({
-    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES],
+    directives: [APP_DIRECTIVES, MDL_COMPONENTS],
     styles: [styles],
     template: template
 })
 export class RevealTheTruth {
+    initialLoading: boolean;
     game;
     question: IQuestion;
     subscribeSource;
     timerSource;
     answerSelected: string;
     isPlayer: boolean;
-    warningMsg: string;
-    errorMsg: string;
     myForm: ControlGroup;
+    displayArray;
 
     constructor(public gameApi: GameApi, public routeParams: RouteParams,
         public formBuilder: FormBuilder, public session: Session,
         public router: Router) {
-        // MDL issue
-        componentHandler.upgradeDom();
 
-        this.buildForm();
         this.isPlayer = session.isPlayer();
 
+        LoadingMaskService.show();
+        this.initialLoading = true;
+
         this.getGame(this.routeParams.get('gameName'))
+            .then(() => {
+                this.initialLoading = false;
+                LoadingMaskService.hide();
+                MdlService.upgradeAllRegistered();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    getGame(gameName: string) {
+        return this.gameApi.get(gameName)
             .then((game) => {
                 this.game = game;
                 this.question = this.getCurrentQuestion(this.game, CURRENT_STATE);
@@ -58,22 +69,32 @@ export class RevealTheTruth {
                     this.router.navigate(NEXT_STATE_ROUTE + game.name);
                 } else {
                     this.subscribe(game.name, this.question);
-                    this.startTimer();
+                    this.timerSource = this.startTimer();
+                    this.createDisplayArray();
                 }
             });
     }
 
-    buildForm() {
-        this.myForm = this.formBuilder.group({
-            answerText: ['', Validators.required]
+    createDisplayArray() {
+        this.displayArray = _.chain(this.question.fakeAnswers)
+            .filter(fakeAnswer => fakeAnswer.selectedBy.length)
+            .sortBy(fakeAnswer => -fakeAnswer.selectedBy.length)
+            .value();
+
+        this.displayArray.push(_.assign({}, this.question.realAnswer, {truth: true}));
+
+        this.displayArray = _.map(this.displayArray, answer => {
+            answer.selectedBy = _.map(answer.selectedBy, this.getPlayerData.bind(this));
+            answer.createdBy = _.map(answer.createdBy, this.getPlayerData.bind(this));
+            return answer;
         });
+
+        console.log('displayArray', this.displayArray);
     }
 
-    getGame(gameName: string) {
-        return this.gameApi.get(gameName)
-            .catch(err => {
-                console.log(err);
-            });
+    getPlayerData(playerId) {
+        let houseObj = {id: 'house'};
+        return _.find(this.game.players, player => player.id === playerId) || houseObj;
     }
 
     subscribe(gameName: string, question) {
@@ -91,10 +112,11 @@ export class RevealTheTruth {
     }
 
     startTimer() {
-        this.timerSource = setTimeout(() => this.gameApi.tick(this.game.name, this.question.id, this.question.state),
-            REVEALING_THE_TRUTH_TIME);
+        setTimeout(() => {
+            this.gameApi.tick(this.game.name, this.question.id, this.question.state)
+        }, REVEALING_THE_TRUTH_TIME);
     }
-    
+
     onDestroy() {
         this.subscribeSource.dispose();
         clearTimeout(this.timerSource);
