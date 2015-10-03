d053bdc4481616d24734f46b0aa2f5eb1cd3d68f
Itay Radotzki <radotzki@gmail.com>
2015-10-03 17:38:01 +0300

    use autoprefixer-loader

--- webpack.config.js
+++ webpack.config.js
@@ -134,7 +134,7 @@ module.exports = {
       { test: /\.json$/,  loader: 'json' },
 
       // Support for CSS as raw text
-      { test: /\.css$/,   loader: 'raw' },
+      { test: /\.css$/,   loader: "raw-loader!autoprefixer-loader" },
 
       // support for .html as raw text
       { test: /\.html$/,  loader: 'raw' },
