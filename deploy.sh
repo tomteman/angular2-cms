82f12b31fefa9030a639971ba3eb9d732af29b42
Itay Radotzki <radotzki@gmail.com>
2015-10-07 19:31:55 +0300

    Bullshit

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # build app on local machine
-cd ~ && cd $POF_CLIENT_PATH && sh build.sh
+cd ~ && cd $BS_CLIENT_PATH && sh build.sh
 
 cd ~
 # delete app directory
-ssh -i $POF_CLIENT_SSH_FILE_PATH $POF_CLIENT_MACHINE_USER@$POF_CLIENT_MACHINE_LOCATION 'rm -rf ~/bullshit-client/*'
+ssh -i $BS_CLIENT_SSH_FILE_PATH $BS_CLIENT_MACHINE_USER@$BS_CLIENT_MACHINE_LOCATION 'rm -rf ~/bullshit-client/*'
 
 # copy dist to remote machine
-scp -r -i $POF_CLIENT_SSH_FILE_PATH $POF_CLIENT_PATH/dist $POF_CLIENT_MACHINE_USER@$POF_CLIENT_MACHINE_LOCATION:~/bullshit-client/dist
+scp -r -i $BS_CLIENT_SSH_FILE_PATH $BS_CLIENT_PATH/dist $BS_CLIENT_MACHINE_USER@$BS_CLIENT_MACHINE_LOCATION:~/bullshit-client/dist
 
 # copy run.sh to remote machine
-scp -i $POF_CLIENT_SSH_FILE_PATH $POF_CLIENT_PATH/run.sh $POF_CLIENT_MACHINE_USER@$POF_CLIENT_MACHINE_LOCATION:~/bullshit-client/run.sh
+scp -i $BS_CLIENT_SSH_FILE_PATH $BS_CLIENT_PATH/run.sh $BS_CLIENT_MACHINE_USER@$BS_CLIENT_MACHINE_LOCATION:~/bullshit-client/run.sh
 
 # copy Docker file to remote machine
-scp -i $POF_CLIENT_SSH_FILE_PATH $POF_CLIENT_PATH/Dockerfile $POF_CLIENT_MACHINE_USER@$POF_CLIENT_MACHINE_LOCATION:~/bullshit-client/Dockerfile
+scp -i $BS_CLIENT_SSH_FILE_PATH $BS_CLIENT_PATH/Dockerfile $BS_CLIENT_MACHINE_USER@$BS_CLIENT_MACHINE_LOCATION:~/bullshit-client/Dockerfile
 
 # run Docker on the remote machine
-ssh -i $POF_CLIENT_SSH_FILE_PATH $POF_CLIENT_MACHINE_USER@$POF_CLIENT_MACHINE_LOCATION 'cd ~/bullshit-client; sh run.sh'
+ssh -i $BS_CLIENT_SSH_FILE_PATH $BS_CLIENT_MACHINE_USER@$BS_CLIENT_MACHINE_LOCATION 'cd ~/bullshit-client; sh run.sh'
